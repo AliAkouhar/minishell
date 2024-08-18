fc5a1f987b908d2b8f42d04d3f4d18eb008745c5
AliAkouhar <akouhar04ali@gmail.com>
2024-08-18 18:48:29 +0100

    push

---
+++ src/execution/builtins/exit.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbazaz <fbazaz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 14:30:57 by fbazaz            #+#    #+#             */
+/*   Updated: 2024/08/16 13:05:48 by fbazaz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/minishell.h"
+
+int	ft_isnumeric(char *str)
+{
+	int	i;
+
+	if (!str || !*str)
+		return (0);
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	len_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
+
+void	ft_exit(char **args)
+{
+	int	count;
+	int	exit_code;
+
+	printf("exit\n");
+	if (!args[1])
+		exit(g_data->exit_status);
+	count = len_args(args);
+	if (!ft_isnumeric(args[1]))
+	{
+		exit_func(NUMERIC_ARGS, args[1]);
+		exit(2);
+	}
+	exit_code = ft_atoi(args[1]);
+	if (count > 2)
+	{
+		exit_func(EXIT_TOO_ARG, NULL);
+		g_data->exit_status = 1;
+		return ;
+	}
+	g_data->exit_status = exit_code;
+	exit(exit_code);
+}
---
+++ src/execution/ft_free.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakouhar <aakouhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 09:54:23 by fbazaz            #+#    #+#             */
+/*   Updated: 2024/08/18 18:36:18 by aakouhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	fre(void *p)
+{
+	if (p)
+	{
+		free(p);
+		p = NULL;
+	}
+	return ;
+}
+
+void	free_2d(char **str)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return ;
+	while (str[++i])
+		fre(str[i]);
+	fre(str);
+}
+
+void	ft_free_struct(t_list **list)
+{
+	t_redir	*tmp;
+	t_list	*list_tmp;
+
+	while (*list)
+	{
+		list_tmp = (*list)->next;
+		if ((*list)->mini_tokens)
+			free_2d((*list)->mini_tokens);
+		if ((*list)->cmd_args)
+			free_2d((*list)->cmd_args);
+		while ((*list)->files)
+		{
+			tmp = (*list)->files->next;
+			if ((*list)->files->type == HERE_DOC)
+			{
+				unlink((*list)->files->filename);
+				fre((*list)->files->lim);
+			}
+			fre((*list)->files->filename);
+			fre((*list)->files);
+			(*list)->files = tmp;
+		}
+		fre(*list);
+		*list = list_tmp;
+	}
+}
+
+int	free_env_ctrl_d(t_env **env)
+{
+	int		i;
+	t_env	*tmp;
+
+	fre(g_data->last_arg);
+	fre(g_data->pwd);
+	fre(g_data->cmd);
+	fre(g_data->path);
+	while (*env)
+	{
+		tmp = (*env)->next;
+		fre((*env)->key);
+		fre((*env)->value);
+		fre(*env);
+		*env = tmp;
+	}
+	i = g_data->exit_status;
+	fre(g_data);
+	return (i);
+}
